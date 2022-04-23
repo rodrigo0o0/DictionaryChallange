f916e5ebbdae2b7502e6c8b6f19c88e381b70a79
Rodrigo Sandeski <sandeski47@gmail.com>
2022-04-23 19:37:10 -0300

    Finished

--- DictionaryChallange/Program.cs
+++ DictionaryChallange/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,39 @@ namespace DictionaryChallange
     {
         static void Main(string[] args)
         {
+            Dictionary<string, int> votes = new Dictionary<string, int>();
+            string path = @"c:\temp\challenge.txt";
+            try
+            {
+                using(StreamReader sr = new StreamReader(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string [] line = sr.ReadLine().Split(',');
+                        string name = line[0];
+                        int vote = int.Parse(line[1]);
+                        if (votes.ContainsKey(name))
+                        {
+                            vote += votes[name];
+                            votes[name] = vote;
+                        }
+                        else
+                        {
+                            votes.Add(name, vote);
+                        }
+
+                    }
+                    foreach(var item in votes)
+                    {
+                        Console.WriteLine(item.Key + " : " + item.Value);    
+                        
+                    }
+                }
+            }catch(InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
         }
     }
 }
